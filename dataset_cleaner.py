f213b865d8b59fefaa55621b84bb9cc64e54536a
Infinityworld13 <74107519+Infinityworld13@users.noreply.github.com>
2020-11-23 13:33:28 -0600

    Update dataset_cleaner.py

--- dataset_cleaner.py
+++ dataset_cleaner.py
@@ -5,6 +5,7 @@ There are two main datasets we are working with:
     - Global Average Sea Level
 """
 from typing import Dict
+import netCDF4 as nc
 
 
 ###################################################################################################
@@ -21,7 +22,13 @@ def read_temperature_data() -> Dict[int, float]:
 # Cleanup Sea level data
 ###################################################################################################
 def read_sea_level_data() -> Dict[int, float]:
-    """Return a dictionary where the key is a certain year and the value is the average sea level
+    """Return a dictionary where the key is a certain year and the value is the average sea level change
     for that year.
     """
-    # TODO implement this (you may add helper functions)
+    fn = 'global_timeseries_measures.nc.nc4'
+    ds = nc.Dataset(fn)
+    averages = ds['global_average_sea_level_change'][:]
+    time_sea = ds['time'][:]
+    sea_levels = {i + 1: averages[i] for i in range(len(time_sea))}
+    return sea_levels
+    
